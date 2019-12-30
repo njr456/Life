7c1843ad84fdf3d0f1ae15d93ddf6054a3673d1b
njr456 <59349119+njr456@users.noreply.github.com>
2019-12-30 10:07:29 +0800

    Update TestGit.java

--- src/com/bdqn/test/TestGit.java
+++ src/com/bdqn/test/TestGit.java
@@ -2,6 +2,6 @@ package com.bdqn.test;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("hello word");
     }
 }
